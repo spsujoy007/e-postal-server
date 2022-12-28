5ae1aa4d1d4459b150a2d13408be0871c41f4752
Sujoy Paul <freelancersujoy@gmail.com>
2022-12-28 19:32:32 +0600

    post whats on your mind get and create

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6ke0m0t.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        const postCollection = client.db('e-postal').collection('postcollection');
+        app.post('/posts', async (req, res) => {
+            const post = req.body;
+            const result = await postCollection.insertOne(post)
+            console.log(result);
+            res.send(result)
+        })
+
+        app.get('/posts', async(req, res) => {
+            const query = {};
+            const result = await postCollection.find(query).toArray();
+            res.send(result)
+        })
+    }
+
+    finally{}
+}
+run().catch(error => console.error(error))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('E-Postal Web Server (Backend)')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`E-Postal web server running at ${port}`)
 })
